dde5fec5192a135476991ae771ba0848a486f590
trashpanda2756 <vern@openlabidaho.org>
2026-03-23 21:17:38 -0600

    pushing

--- src/main/java/frc/robot/subsystems/turret/ShotTimeTable.java
+++ src/main/java/frc/robot/subsystems/turret/ShotTimeTable.java
@@ -9,19 +9,19 @@ public class ShotTimeTable {
 
   static {
     // key = distanceMeters, value = flightTimeSeconds
-    table.put(0.00, 1.23);
-    table.put(1.55, 1.23);
-    table.put(1.86, 1.24);
-    table.put(2.14, 1.31);
-    table.put(2.45, 1.28);
-    table.put(2.73, 1.34);
-    table.put(3.06, 1.38);
-    table.put(3.42, 1.40);
-    table.put(3.74, 1.21);
-    table.put(4.05, 1.36);
-    table.put(4.44, 1.34);
-    table.put(4.90, 1.42);
-    table.put(5.34, 1.30);
+    table.put(0.00, 1.22);
+    table.put(1.55, 1.22);
+    table.put(1.86, 1.22);
+    table.put(2.14, 1.25);
+    table.put(2.45, 1.25);
+    table.put(2.73, 1.25);
+    table.put(3.06, 1.27);
+    table.put(3.42, 1.27);
+    table.put(3.74, 1.27);
+    table.put(4.05, 1.32);
+    table.put(4.44, 1.33);
+    table.put(4.90, 1.35);
+    table.put(5.34, 1.37);
     table.put(7.50, 1.5);
     table.put(10.00, 1.60);
     table.put(15.00, 1.70);
