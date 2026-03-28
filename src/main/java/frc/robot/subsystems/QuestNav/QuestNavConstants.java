b03412d2fe1375cffe0e3d10a0b67fd9ba5b6ba4
trashpanda2756 <vern@openlabidaho.org>
2026-03-28 11:20:08 -0600

    Update at 'Sat Mar 28 11:20:07 MDT 2026'

--- src/main/java/frc/robot/subsystems/QuestNav/QuestNavConstants.java
+++ src/main/java/frc/robot/subsystems/QuestNav/QuestNavConstants.java
@@ -33,7 +33,7 @@ public class QuestNavConstants {
           Units.inchesToMeters(
               FieldConstants.FIELD_LENGTH_INCHES
                   - ROBOT_WIDTH_INCHES_BUMPER_HALF_DOUBLE
-                  - ROBOT_TO_QUEST_INCHES_X_DOUBLE),
+                  + ROBOT_TO_QUEST_INCHES_X_DOUBLE),
           Units.inchesToMeters(ROBOT_LENGTH_INCHES_BUMPER__HALF_DOUBLE),
           Units.inchesToMeters(ROBOT_TO_QUEST_INCHES_Z_DOUBLE),
           new Rotation3d(0, 0, Units.degreesToRadians(180)));
